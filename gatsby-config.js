e4df17070c42e77a526c109402d1cbb6c39a1460
james <james.coates@me.com>
2019-08-20 22:08:33 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  siteMetadata: {
+    title: `James Coates`,
+    description: `Full Stack Web Developer`,
+    author: `James Coates`,
+  },
+  pathPrefix: "/portfolio",
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Limelight`,
+          `Roboto\:300,400,700,900`
+        ],
+        display: 'swap'
+      }
+    }
+  ],
+}
